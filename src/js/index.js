9d76e05f1d809649eb1d6da01f483b1055bea495
John Rohan <john.rohan87@gmail.com>
2020-06-30 02:02:29 +0000

    updating the light

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,5 @@
 //import react into the bundle
-import React from "react";
+import React, { useState, useEffect } from "react";
 import ReactDOM from "react-dom";
 
 //include bootstrap npm library into the bundle
@@ -11,5 +11,31 @@ import "../styles/index.scss";
 //import your own components
 import Layout from "./layout";
 
+const MyPage = () => {
+	const [color, setColor] = useState(null);
+
+	return (
+		<div className="container centered w-25 h-10 mx-auto pt-5 rounded border border-dark">
+			<div className="btn-group-vertical">
+				<div className={color === "red" ? "red active" : "red inactive"} onClick={() => setColor("red")}>
+					red
+				</div>
+				<div
+					className={color === "yellow" ? "yellow active" : "yellow inactive"}
+					onClick={() => setColor("yellow")}>
+					yellow
+				</div>
+				<div
+					className={color === "green" ? "green active" : "green inactive"}
+					onClick={() => setColor("green")}>
+					green
+				</div>
+
+				<div>Light Status {color}</div>
+			</div>
+		</div>
+	);
+};
+
 //render your react application
-ReactDOM.render(<Layout />, document.querySelector("#app"));
+ReactDOM.render(<MyPage />, document.querySelector("#app"));
